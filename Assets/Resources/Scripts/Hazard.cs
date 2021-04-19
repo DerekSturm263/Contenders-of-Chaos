31f00c561d5e3cdd68ff10aff49b28490810ccd8
Konner Lewis <1615931@mukilteo.wednet.edu>
2021-04-19 12:41:27 -0700

    Update Hazard.cs and Hazard.cs.meta

---
+++ Assets/Resources/Scripts/Hazard.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : MonoBehaviour
+{
+
+    CharacterController fpsinput;
+    public float power;
+
+    private void Awake()
+    {
+        fpsinput = GameObject.Find("PlayerMovement").GetComponent<CharacterController>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collisionData)
+    {
+        if (collisionData.gameObject.tag == "Player")
+        {
+            fpsinput.movement.velocity.y = 0;
+            fpsinput.movement.velocity = fpsinput.movement.velocity + transform.up * power;
+        }
+    }
+
+}
